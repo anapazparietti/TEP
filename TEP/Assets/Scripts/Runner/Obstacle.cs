6a7ca94e0a6897ccc2383f49a6b6973af0701e84
ana <anapazparietti@gmail.com>
2024-11-06 17:03:54 -0300

    avances 17:03

---
+++ TEP/Assets/Scripts/Runner/Obstacle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+public GameObject onDestroyEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+private void OnTriggerEnter(Collider other) {
+
+  if(other.CompareTag("Player")){
+    //cuando el jugador entre en esta Trigger zone, se va a destruir el collectible
+  Destroy(gameObject);
+//para que se produzca el efecto no solo hay que agregar el archivo, sino que además hay que Instanciarlo:
+Instantiate(onDestroyEffect, transform.position, transform.rotation);
+  }
+  
+}
+
+}
+
--- TEP/Assets/Scripts/Runner/Player.cs
+++ TEP/Assets/Scripts/Runner/Player.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,22 +9,43 @@ public class Player : MonoBehaviour
 
     private Rigidbody rb;
     private float inputHorizontal;
+    private float originalForwardSpeed;  // Almacena la velocidad original
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        originalForwardSpeed = forwardSpeed;  // Guardamos la velocidad original
     }
 
     private void Update()
     {
         // Captura la entrada horizontal del usuario
         inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
     }
 
     private void FixedUpdate()
     {
         // Aplica movimiento automático en el eje Z y controlado en el eje X
         rb.linearVelocity = new Vector3(inputHorizontal * speed, rb.linearVelocity.y, forwardSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            StartCoroutine(ReduceSpeedTemporarily());
+        }
+    }
+
+    private IEnumerator ReduceSpeedTemporarily()
+    {
+        // Reduce la velocidad hacia adelante
+        forwardSpeed = originalForwardSpeed / 2;  // Reduce la velocidad a la mitad (ajustable)
+
+        // Espera 1 segundo
+        yield return new WaitForSeconds(1f);
+
+        // Restaura la velocidad original
+        forwardSpeed = originalForwardSpeed;
     }
 }
