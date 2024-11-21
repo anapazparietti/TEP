860d1b2daa23e9754e5b84d9373866dd2186bc54
ana <anapazparietti@gmail.com>
2024-11-21 11:10:09 -0300

    v

--- TEP/Assets/Scripts/Sincro/SyncSequenceTrigger.cs
+++ TEP/Assets/Scripts/Sincro/SyncSequenceTrigger.cs
@@ -1,32 +1,21 @@
+using System.Collections.Generic; // Necesario para listas
 using UnityEngine;
-using System.Collections.Generic;
-using Assets.Scripts.Sincro;
-
 
 public class SyncSequenceTrigger : MonoBehaviour
 {
+    [Header("Nuevas Secuencias para Asignar")]
     public string[] sequence1;
     public string[] sequence2;
 
     private void OnTriggerEnter(Collider other)
     {
-<<<<<<< Updated upstream
+        if (other.CompareTag("Player"))
         if (other.CompareTag("Player2"))
-=======
-        var playerController = other.GetComponent<PlayerSyncController>();
-        if (playerController != null)
->>>>>>> Stashed changes
         {
-            SyncManager syncManager = FindObjectOfType<SyncManager>();
-            syncManager.sequences = new List<string[]>
-            {
-                sequence1,
-                sequence2
-            };
-
-            Debug.Log($"Secuencias actualizadas para {other.name}");
+            SyncManager2 syncManager = other.GetComponent<SyncManager2>();
+            if (syncManager != null);
+            }
         }
     }
 
     
-}
