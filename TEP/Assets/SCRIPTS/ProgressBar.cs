021b0ef6b55cbfef17b522e9b161ca501d61e2e2
ana <anapazparietti@gmail.com>
2024-11-26 10:45:26 -0300

    intento de mapa

---
+++ TEP/Assets/SCRIPTS/ProgressBar.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    // necesito que mientras avanza el personaje vaya aumentando o deteniendose dependiendo la distancia del personaje
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
