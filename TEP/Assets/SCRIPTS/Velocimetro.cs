ed63344100d1468bf8bbf9e6ee7cb429a05b5bce
Guada López <guadilopez.995@gmail.com>
2024-11-26 10:42:31 -0300

    Velocimetro

---
+++ TEP/Assets/SCRIPTS/Velocimetro.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Velocimetro : MonoBehaviour
+{
+    public Rigidbody MiDino;
+    public float Max_speed = 200f;
+    public float MinSpeedArrowAngle;
+    public float MaxSpeedArrowAngle;
+
+    [Header("UO")]
+
+    public RectTransform Arrow;
+    public float speed = 0.0f;
+
+    void Update()
+    {
+        speed = MiDino.velocity.magnitude * 3.6f;
+
+        if(Arrow != null)
+        {
+            Arroe.localEulerAngles = new Vector3(0,0, Mathf.Lerp(MinSpeedArrowAngle, MaxSpeedArrowAngle, speed / Max_speed));
+        }
+    }
+}
