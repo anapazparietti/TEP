21d1f6eb5ae0bdaf724119c2c2d0d72d279c1f2e
Guada López <guadilopez.995@gmail.com>
2024-11-26 09:18:34 -0300

    Temporizador

---
+++ TEP/Assets/SCRIPTS/Temporizador.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temporizador : MonoBehaviour
+{
+    [SerializeField] int min, seg;
+    [SerializeField] Text tiempo;
+    
+    private float restante;
+
+    private bool enMovimiento;
+
+    private void Awake(){
+
+        restante = (min * 60) + seg;
+        enMovimiento = true;
+    }
+
+    void Update()
+    {
+      if(enMovimiento)
+      {
+        restante -= Time.deltaTime;
+        if(restante < 1)
+        {
+            enMovimiento = false;
+            //pantalla perdiste
+        }
+        int tempMin = Mathf.FloorToInt(restante / 60);
+        int tempSeg = Mathf.FloorToInt(restante % 60);
+        tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+      }
+
+    }
+}
